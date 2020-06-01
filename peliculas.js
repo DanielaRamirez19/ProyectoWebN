f7323d941bd40d28e0e9cb139826f92f67f5a8dc
daniela-morales <54753171+daniela-morales@users.noreply.github.com>
2020-06-01 17:13:22 +0200

    codigo de programacion y sillas

--- peliculas.js
+++ peliculas.js
@@ -1,12 +1,42 @@
 $(document).on('ready', function () {
-    $.getJSON("catalogo.json", function (json) {
+    $.getJSON("https://localhost:44359/api/Cine?descri=", function (json) {
         $('#select').empty();
         $('#select').append($('<option>').text("Select"));
         $.each(json, function (i, obj) {
-            $('#usuarios').append($('<option>').text(obj.Nombre).attr('value', obj.Sala));
+            $('#usuarios').append($('<option>').text(obj.Nombre).attr('value', obj.Descripcion));
         });
     });
 });
+
+
+function seleccionFecha() {  
+    var e = document.getElementById("usuarios");
+    var strUser = e.options[e.selectedIndex].text;
+    var api = "https://localhost:44359/api/Cine?pelicula=" + strUser;
+    $.getJSON(api, function (json) {
+        $('#select').empty();
+        $('#select').append($('<option>').text("Select"));
+        $.each(json, function (i, obj) {
+            $('#usuariosss').append($('<option>').text(obj.Dia).attr('value', obj.Pelicula));
+        });
+    });
+}
+
+function seleccionHora() {  
+    var p = document.getElementById("usuarios");
+    var pelicula = p.options[p.selectedIndex].text;
+    var d = document.getElementById("usuarios");
+    var dia = d.options[d.selectedIndex].text;
+    var api = "https://localhost:44359/api/Cine?pelicula=" + pelicula + " &Dia=" + dia;
+    console.log(api);
+    $.getJSON(api, function (json) {
+        $('#select').empty();
+        $('#select').append($('<option>').text("Select"));
+        $.each(json, function (i, obj) {
+            $('#usuarioss').append($('<option>').text(obj.Dia).attr('value', obj.Pelicula));
+        });
+    });
+}
 
 window.onload = function traerDatos() {
     var xhr = new XMLHttpRequest();
@@ -14,7 +44,7 @@ window.onload = function traerDatos() {
     
     xhr.addEventListener("readystatechange", function() {
       if(this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
+        //console.log(this.responseText);
         let datos = JSON.parse(this.responseText);
         let res= document.querySelector('#res');
             res.innerHTML = '';
@@ -22,15 +52,16 @@ window.onload = function traerDatos() {
             for(let item of datos){
                 res.innerHTML += `
                     <tr>
-                        <td>${item.Nombre}</td>
-                        <td>${item.Duracion}</td>
-                        <td>${item.Director}</td>
+                        <td>${item.Pelicula}</td>
+                        <td>${item.Sala}</td>
+                        <td>${item.Dia}</td>
+                        <td>${item.Hora}</td>
                     </tr>
                 `
             }
       }
     });
     
-    xhr.open("GET", "https://localhost:44359/api/Cine");
+    xhr.open("GET", "https://localhost:44359/api/Cine?progr=");
     xhr.send();
 }
